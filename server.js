26d54505a3acd689daed1b8cac8a3f31c3082299
Mohamed SADAT <moh12594@gmail.com>
2017-07-30 23:02:37 +0200

    Configue the api

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const http = require('http');
 const app = express();
 
+const api = require('./server/routes/api');
+
 // Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -12,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Set the api routes
-//app.use('/api', api);
+app.use('/api', api);
 
 // return other routes to Angular index file..
 app.get('*', (req, res) => {
