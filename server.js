2c7bbc1244b574a7858d13759026fe7f4f6cd324
Mohamed SADAT <moh12594@gmail.com>
2017-07-30 22:22:33 +0200

    Setup express server

--- server.js
+++ server.js
@@ -3,3 +3,27 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const app = express();
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set the api routes
+//app.use('/api', api);
+
+// return other routes to Angular index file..
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+// Set the port 
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+// Create the HTTP Server
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost: ${port}`));
